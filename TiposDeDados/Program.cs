26e8330dcb3528e7a1cb45a03677b3b854c5a55a
nickmenegussi <nicolasmramos09@gmail.com>
2025-06-24 16:44:15 -0300

    feat: primeira aula sobre tipos de dados e imprimindo e pedindo resultados no console

---
+++ TiposDeDados/Program.cs
@@ -0,0 +1,17 @@
+﻿// tipos de dados inteiros, double, float, decimal, strings, boolean em c#
+int numero = 4; // inteiro
+double segundoNumero = 3.304;
+char primeiraLetraNome = 'L';
+String nomeCompleto = "Nicolas Menegussi Ramos";
+bool isOverEighteenYears = false;
+
+Console.WriteLine(numero);
+Console.WriteLine(segundoNumero);
+Console.WriteLine(primeiraLetraNome);
+Console.WriteLine(nomeCompleto);
+Console.WriteLine(isOverEighteenYears);
+
+// pedindo pro usuario digitar algo e depois imprimir na tela o resultado.
+Console.WriteLine("Digite seu nome: ");
+String nomeUser = Console.ReadLine();
+Console.WriteLine("Seja Bem-Vindo, " + nomeUser + "!");
